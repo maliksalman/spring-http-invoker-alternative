72583917b08fb37877bb5c86b0742329c6dd8223
Salman Malik <salman.the.malik@gmail.com>
2024-10-02 12:33:30 -0400

    Adding spring-framework 5.x compatible client-side alternative

--- client/src/main/java/com/example/httpinvokeralternative/client/HttpWidgetService.java
+++ client/src/main/java/com/example/httpinvokeralternative/client/HttpWidgetService.java
@@ -2,19 +2,45 @@ package com.example.httpinvokeralternative.client;
 
 import org.example.httpinvokeralternative.library.Widget;
 import org.example.httpinvokeralternative.library.WidgetService;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.PostExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
-public interface HttpWidgetService extends WidgetService {
+@Service
+public class HttpWidgetService implements WidgetService {
 
-    @GetExchange
-    List<Widget> listWidgets();
+    private final RestTemplate restTemplate;
+    private final String baseUrl;
 
-    @PostExchange
-    long createWidget(@RequestParam("name") String name,
-                      @RequestParam("width") int width,
-                      @RequestParam("height") int height);
+    public HttpWidgetService(@Value("${service-urls.widget}") String baseUrl) {
+        this.baseUrl = baseUrl;
+        this.restTemplate = new RestTemplate();
+    }
+
+    @Override
+    public List<Widget> listWidgets() {
+        ParameterizedTypeReference<List<Widget>> typeRef = new ParameterizedTypeReference<List<Widget>>() {};
+        return restTemplate
+                .exchange(baseUrl, HttpMethod.GET, new HttpEntity<>(null), typeRef)
+                .getBody();
+    }
+
+    @Override
+    public long createWidget(String name, int width, int height) {
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(baseUrl);
+        uriBuilder.queryParam("name", name);
+        uriBuilder.queryParam("width", width);
+        uriBuilder.queryParam("height", height);
+
+        return restTemplate
+                .exchange(uriBuilder.toUriString(), HttpMethod.POST, new HttpEntity<>(null), Long.class)
+                .getBody();
+    }
 }
